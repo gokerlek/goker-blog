8894317a2a2d052925ef695aa46d2f7206540afb
Goker zafer <gokerlek@gmail.com>
2021-02-18 20:29:49 +0300

    Mission completed

--- src/day1.js
+++ src/day1.js
@@ -1,56 +1,60 @@
 export const Day1 = () => {
     return <div className="day1">
         <p>
-            I have installed NVM by running the following command on the terminal <br></br>
-
-                wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
-
-                When the installation was finished, I listed the files by entering the command below.
-
-                ls -al ~/
-
-                I executed the bashrc file from the incoming list with the following command.
-
-                cat ~/.bashrc
-
-                I downloaded the NodeJs latest version by entering the command below.
-
-                nvm install v15.8.0
-
-                I entered the following commands to check
-
-                npm -v
-
-                node-v
-
-                To create a Goker Blog project file, it is necessary to enter the projects file. I created my file in the projects folder by entering the commands below.
-                cd projects
-
-                npx create-react-app goker-blog
-
-                To install the default react app in the goker blog file, go to the following command
-
-                cd goker-blog
-
-                npm start
-
-                When the process is over, localhost: 3000 opens in the browser. The default react app works locally.
-
-                Open a new terminal and enter the following commands.
-
-                cd projects
-
-                code goker-blog
-
-                Thus, the app I created in VS opens.
-
-                After that I opened a new repostroy on GitHub and mapped it to VS.
-
-                Then I signed in to vercel.com with my github account.
-
-                Then I uploaded my goker-blog project on github.
-
-                Now the changes I made in my project can be viewed online.
+            <div>
+                <p>I have installed NVM by running the following command on the terminal <br></br>
+                </p>
+                <p>wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+        </p>
+                <p>When the installation was finished, I listed the files by entering the command below.
+        </p>
+                <p>ls -al ~/
+        </p>
+                <p>I executed the bashrc file from the incoming list with the following command.
+        </p>
+                <p>cat ~/.bashrc
+        </p>
+                <p>I downloaded the NodeJs latest version by entering the command below.
+        </p>
+                <p>nvm install v15.8.0
+        </p>
+                <p>I entered the following commands to check
+        </p>
+                <p>npm -v
+        </p>
+                <p>node-v
+        </p>
+                <p>To create a Goker Blog project file, it is necessary to enter the projects file. I created my file in the projects folder by entering the commands below.
+        </p>
+                <p>cd projects
+        </p>
+                <p>npx create-react-app goker-blog
+        </p>
+                <p>To install the default react app in the goker blog file, go to the following command
+        </p>
+                <p>cd goker-blog
+        </p>
+                <p> npm start
+        </p>
+                <p> When the process is over, localhost: 3000 opens in the browser. The default react app works locally.
+        </p>
+                <p>Open a new terminal and enter the following commands.
+        </p>
+                <p>cd projects
+        </p>
+                <p>code goker-blog
+        </p>
+                <p>Thes, the app I created in VS opens.
+        </p>
+                <p>After that I opened a new repostroy on GitHub and mapped it to VS.
+        </p>
+                <p>Then I signed in to vercel.com with my github account.
+        </p>
+                <p>Then I uploaded my goker-blog project on github.
+        </p>
+                <p>Now the changes I made in my project can be viewed online.
+        </p>
+            </div>
         </p>
     </div>;
 }
