de08cf4dd4e0e7fe0ff356bdcabe5241d8fec35b
Goker zafer <gokerlek@gmail.com>
2021-02-18 20:53:21 +0300

    Mission completed correctly

--- src/day1.js
+++ src/day1.js
@@ -1,60 +1,59 @@
 export const Day1 = () => {
     return <div className="day1">
-        <p>
-            <div>
-                <p>I have installed NVM by running the following command on the terminal <br></br>
-                </p>
-                <p>wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+
+        <div>
+            <p>I have installed NVM by running the following command on the terminal
         </p>
-                <p>When the installation was finished, I listed the files by entering the command below.
+            <p>wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
         </p>
-                <p>ls -al ~/
+            <p>When the installation was finished, I listed the files by entering the command below.
         </p>
-                <p>I executed the bashrc file from the incoming list with the following command.
+            <p>ls -al ~/
         </p>
-                <p>cat ~/.bashrc
+            <p>I executed the bashrc file from the incoming list with the following command.
         </p>
-                <p>I downloaded the NodeJs latest version by entering the command below.
+            <p>cat ~/.bashrc
         </p>
-                <p>nvm install v15.8.0
+            <p>I downloaded the NodeJs latest version by entering the command below.
         </p>
-                <p>I entered the following commands to check
+            <p>nvm install v15.8.0
         </p>
-                <p>npm -v
+            <p>I entered the following commands to check
         </p>
-                <p>node-v
+            <p>npm -v
         </p>
-                <p>To create a Goker Blog project file, it is necessary to enter the projects file. I created my file in the projects folder by entering the commands below.
+            <p>node-v
         </p>
-                <p>cd projects
+            <p>To create a Goker Blog project file, it is necessary to enter the projects file. I created my file in the projects folder by entering the commands below.
         </p>
-                <p>npx create-react-app goker-blog
+            <p>cd projects
         </p>
-                <p>To install the default react app in the goker blog file, go to the following command
+            <p>npx create-react-app goker-blog
         </p>
-                <p>cd goker-blog
+            <p>To install the default react app in the goker blog file, go to the following command
         </p>
-                <p> npm start
+            <p>cd goker-blog
         </p>
-                <p> When the process is over, localhost: 3000 opens in the browser. The default react app works locally.
+            <p> npm start
         </p>
-                <p>Open a new terminal and enter the following commands.
+            <p> When the process is over, localhost: 3000 opens in the browser. The default react app works locally.
         </p>
-                <p>cd projects
+            <p>Open a new terminal and enter the following commands.
         </p>
-                <p>code goker-blog
+            <p>cd projects
         </p>
-                <p>Thes, the app I created in VS opens.
+            <p>code goker-blog
         </p>
-                <p>After that I opened a new repostroy on GitHub and mapped it to VS.
+            <p>Thes, the app I created in VS opens.
         </p>
-                <p>Then I signed in to vercel.com with my github account.
+            <p>After that I opened a new repostroy on GitHub and mapped it to VS.
         </p>
-                <p>Then I uploaded my goker-blog project on github.
+            <p>Then I signed in to vercel.com with my github account.
         </p>
-                <p>Now the changes I made in my project can be viewed online.
+            <p>Then I uploaded my goker-blog project on github.
         </p>
-            </div>
+            <p>Now the changes I made in my project can be viewed online.
         </p>
+        </div>
     </div>;
 }
