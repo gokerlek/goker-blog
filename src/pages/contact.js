0a028c7269be89bb9adfb8197bd8fe48aa1ba2f0
Goker zafer <gokerlek@gmail.com>
2021-03-06 14:30:16 +0300

    image link created

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,5 +1,24 @@
+import { ImageWrapper } from '../components/image-wrapper/image-wrapper';
+import instagram from '../images/instagram.svg';
+import github from '../images/github.svg';
+import twitter from '../images/twitter.svg';
+import linkedin from '../images/linkedin.svg';
+import { Link } from "react-router-dom";
+
 export const ContactPage = () => {
     return <div>
-        Cotact me
-        </div>
+        Contact me
+        <ImageWrapper>
+            <a href="https://www.instagram.com/gokerlek/"> <img src={instagram} /> </a>
+        </ImageWrapper>
+        <ImageWrapper>
+            <a href="https://github.com/gokerlek"> <img src={github} /> </a>
+        </ImageWrapper>
+        <ImageWrapper>
+            <a href="https://twitter.com/gokerzfr"> <img src={twitter} /> </a>
+        </ImageWrapper>
+        <ImageWrapper>
+            <a href="https://www.linkedin.com/in/gokerzafer/"> <img src={linkedin} /> </a>
+        </ImageWrapper>
+    </div>
 }
