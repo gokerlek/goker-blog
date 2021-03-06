4f59ec7f4d6999f561e28bc6e05b10551119dacd
Goker zafer <gokerlek@gmail.com>
2021-03-06 22:52:13 +0300

    contact me page finished

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,24 +2,28 @@ import { ImageWrapper } from '../components/image-wrapper/image-wrapper';
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
-import { RiInstagramFill } from "react-icons/ri";
+import { FaInstagram } from "react-icons/fa";
 export const ContactPage = () => {
     return <div>
         <h3>Contact me </h3>
+        <p>Hello,</p>
+        <p>I am a web developer</p>
+        <p>If you want to do a project with me</p>
+        <p>Let's keep in touch</p>
         <p>+90 542 323 92 48</p>
         <p>gokerlek@gmail.com </p>
         <p>Urla-IZMIR-TURKEY</p>
         <ImageWrapper>
-            <a href="https://www.instagram.com/gokerlek/"> <RiInstagramFill /> </a>
+            <a href="https://www.instagram.com/gokerlek/"> <FaInstagram className="social" /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://github.com/gokerlek"> <FaGithub /> </a>
+            <a href="https://github.com/gokerlek"> <FaGithub className="social" /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://twitter.com/gokerzfr"> <FaTwitter /> </a>
+            <a href="https://twitter.com/gokerzfr"> <FaTwitter className="social" /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://www.linkedin.com/in/gokerzafer/"> <FaLinkedinIn /> </a>
+            <a href="https://www.linkedin.com/in/gokerzafer/"> <FaLinkedinIn className="social" /> </a>
         </ImageWrapper>
     </div>
 }
