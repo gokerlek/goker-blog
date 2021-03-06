025798de70a95e5145dd839c671dd69ddd1c0225
Goker zafer <gokerlek@gmail.com>
2021-03-06 20:16:59 +0300

    edit2

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,22 +1,25 @@
 import { ImageWrapper } from '../components/image-wrapper/image-wrapper';
-import instagram from '../images/instagram.svg';
-import github from '../images/github.svg';
-import twitter from '../images/twitter.svg';
-import linkedin from '../images/linkedin.svg';
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
 export const ContactPage = () => {
     return <div>
-        Contact me
+        <h3>Contact me </h3>
+        <p>+90 542 323 92 48</p>
+        <p>gokerlek@gmail.com </p>
+        <p>Urla-IZMIR-TURKEY</p>
         <ImageWrapper>
-            <a href="https://www.instagram.com/gokerlek/"> <img src={instagram} /> </a>
+            <a href="https://www.instagram.com/gokerlek/"> <RiInstagramFill /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://github.com/gokerlek"> <img src={github} /> </a>
+            <a href="https://github.com/gokerlek"> <FaGithub /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://twitter.com/gokerzfr"> <img src={twitter} /> </a>
+            <a href="https://twitter.com/gokerzfr"> <FaTwitter /> </a>
         </ImageWrapper>
         <ImageWrapper>
-            <a href="https://www.linkedin.com/in/gokerzafer/"> <img src={linkedin} /> </a>
+            <a href="https://www.linkedin.com/in/gokerzafer/"> <FaLinkedinIn /> </a>
         </ImageWrapper>
     </div>
 }
