5e58094ba8bce9eb936d20042d1a007d414ea270
Goker zafer <gokerlek@gmail.com>
2021-03-07 22:53:39 +0300

    edit

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,11 +1,32 @@
+import { PageHeader } from '../components/page-header/page-header'
+import { useState } from 'react'
 import { ProjectContent } from '../components/project-content/project-content'
 export const ProjectsPage = () => {
+    const [datas, setDatas] = useState([
+        {
+            title: "Göker Blog",
+            usedTechnology: "React, Css, HTML",
+            summery: "My first project where I shared the steps I took while becoming a developer.",
+            projectlink: "/day1",
+        },
+        {
+            title: "Göker Blog",
+            usedTechnology: "React, Css, HTML",
+            summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
+            projectlink: "/day1",
+        },
+        {
+            title: "Göker Blog",
+            usedTechnology: "React, Css, HTML",
+            summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
+            projectlink: "/day1",
+        },
+    ])
     return <div>
-        <h1>My projects</h1>
-        <ProjectContent title="Göker Blog" usedTechnology="React, Css, HTML" summery="summurryfvfv" projectlink="/day1"></ProjectContent>
-        <ProjectContent title="React: Very First Steps" usedTechnology="React" summery="summurryfvfv" projectlink="/day1"></ProjectContent>
-        <ProjectContent title="React: Very First Steps" usedTechnology="React" summery="summurryfvfv" projectlink="/day1"></ProjectContent>
-        <ProjectContent title="React: Very First Steps" usedTechnology="React" summery="summurryfvfv" projectlink="/day1"></ProjectContent>
+        <PageHeader>My projects</PageHeader>
+        {datas.map(data => (
+            <ProjectContent title={data.title} usedTechnology={data.usedTechnology} summery={data.summery} projectlink={data.projectlink}></ProjectContent>
+        ))}
 
     </div>
 }
