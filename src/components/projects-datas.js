eceb624fce6794b81372114a60df6b3d48aa22cb
Goker zafer <gokerlek@gmail.com>
2021-03-08 14:17:39 +0300

    ProjectsDatas created

---
+++ src/components/projects-datas.js
@@ -0,0 +1,20 @@
+export const ProjectsDatas = [
+    {
+        title: "Göker Blog",
+        usedTechnology: "React, Css, HTML",
+        summery: "My first project where I shared the steps I took while becoming a developer.",
+        projectlink: "/",
+    },
+    {
+        title: "Göker Blog",
+        usedTechnology: "React, Css, HTML",
+        summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
+        projectlink: "/day2",
+    },
+    {
+        title: "Göker Blog",
+        usedTechnology: "React, Css, HTML",
+        summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
+        projectlink: "/day3",
+    },
+]
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,31 +1,18 @@
 import { PageHeader } from '../components/page-header/page-header'
 import { useState } from 'react'
 import { ProjectContent } from '../components/project-content/project-content'
+import { ProjectsDatas } from '../components/projects-datas'
 export const ProjectsPage = () => {
-    const [datas, setDatas] = useState([
-        {
-            title: "Göker Blog",
-            usedTechnology: "React, Css, HTML",
-            summery: "My first project where I shared the steps I took while becoming a developer.",
-            projectlink: "/",
-        },
-        {
-            title: "Göker Blog",
-            usedTechnology: "React, Css, HTML",
-            summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
-            projectlink: "/day2",
-        },
-        {
-            title: "Göker Blog",
-            usedTechnology: "React, Css, HTML",
-            summery: "summurryfvfv summurryfvfv summurryfvfv summurryfvfv summurryfvfv",
-            projectlink: "/day3",
-        },
-    ])
+    const [datas, setDatas] = useState([{ ProjectsDatas }])
     return <div>
-        <PageHeader>My projects</PageHeader>
+        <PageHeader> My projects </PageHeader>
         {datas.map(data => (
-            <ProjectContent title={data.title} usedTechnology={data.usedTechnology} summery={data.summery} projectlink={data.projectlink}></ProjectContent>
+            <ProjectContent
+                title={data.title}
+                usedTechnology={data.usedTechnology}
+                summery={data.summery}
+                projectlink={data.projectlink}>
+            </ProjectContent>
         ))}
 
     </div>
