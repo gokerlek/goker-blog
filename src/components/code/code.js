36c2e8e58fbb759b7be22ec6f1af6f3b10a09c3d
Goker zafer <gokerlek@gmail.com>
2021-03-12 21:22:54 +0300

    try some thing

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -1,6 +1,7 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
 export const Code = ({ children, language = 'javascript' }) => {
+  xonokai.borderRadius = '100px';
   return (
     <SyntaxHighlighter language={language} style={xonokai}>
       {children}
