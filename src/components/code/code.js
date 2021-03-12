32b00483ff46eedb3855e6adb95d6b694f61d471
Goker zafer <gokerlek@gmail.com>
2021-03-13 00:21:21 +0300

    SyntaxHighlighter finished

--- src/components/code/code.js
+++ src/components/code/code.js
@@ -1,9 +1,11 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { xonokai } from 'react-syntax-highlighter/dist/esm/styles/prism';
 export const Code = ({ children, language = 'javascript' }) => {
-  xonokai.borderRadius = '100px';
   return (
-    <SyntaxHighlighter language={language} style={xonokai}>
+    <SyntaxHighlighter
+      language={language}
+      style={xonokai}
+      customStyle={{ borderRadius: '10px', backgroundColor: '#292929' }}>
       {children}
     </SyntaxHighlighter>
   );
