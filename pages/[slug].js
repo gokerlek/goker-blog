f0b686ee5a7aec3202d29f9f69c2a6c46491ca31
Goker zafer <gokerlek@gmail.com>
2021-03-20 22:53:23 +0300

    pages datas conneced with jsons

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,11 +1,30 @@
 import { Layout } from '../components/layout';
 import { Container } from '../components/container';
 import { getPage, getSlugsOfPages } from '../api/pages';
+import ReactMarkdown from 'react-markdown';
 
 const Page = ({ page, menu, footer }) => {
+  console.log(page);
   return (
     <Layout menu={menu.menuItems} footer={footer}>
-      <Container>Page</Container>
+      <Container>
+        <div key={page.id}>
+          {page.banner ? <img src={page.banner} /> : null}
+          <h1>{page.title}</h1>
+          <div>
+            {page.blocks.map((block) => (
+              <div key={block.id}>
+                {block.title ? (
+                  <div className="block-title">{block.title}</div>
+                ) : null}
+                {block.template === 'text-block' ? (
+                  <ReactMarkdown>{block.content}</ReactMarkdown>
+                ) : null}
+              </div>
+            ))}
+          </div>
+        </div>
+      </Container>
     </Layout>
   );
 };
