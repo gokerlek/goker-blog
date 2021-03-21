e0c8c20a999c6a66ebffced0b2b0edeaa07ad9db
Goker zafer <gokerlek@gmail.com>
2021-03-21 23:59:51 +0300

    all datas connected

--- api/home.js
+++ api/home.js
@@ -5,8 +5,13 @@ export async function getHome() {
   const page = await API.get(`home`).catch((err) => {
     console.error(err);
   });
+  page.latest_post.blog_post = {
+    path: page.latest_post.blog_post.replace('.json','').replace('cms/',''), 
+    content: await API.get(page.latest_post.blog_post.replace('cms','').replace('.json','')
+    )}
   const menu = await getMenu();
   const footer = await getFooter();
+  
 
   return {
     page,
