b1c0a71711050c6055fed41238f0364c64311d00
Goker zafer <gokerlek@gmail.com>
2021-03-23 23:19:52 +0300

    blog style complated for home page

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,12 @@ import { Container } from '../components/container';
 import { getHome } from '../api/home';
 import ReactMarkdown from 'react-markdown';
 import { Code } from '../components/code';
+import BlogPostWithImage from '../components/blog-post-with-image';
 
 const Home = ({ page, menu, footer }) => {
   return (
     <Layout footer={footer} menu={menu.menuItems}>
-      <Container>
+      <Container width="960px">
         <h1>{page.title}</h1>
         <div>
           <h3>{page.intro.title}</h3>
@@ -17,23 +18,29 @@ const Home = ({ page, menu, footer }) => {
         <div>
           <h3>{page.latest_post.title}</h3>
           <ReactMarkdown>{page.latest_post.description}</ReactMarkdown>
-          <Link
-            as={page.latest_post.blog_post.path.replace('blog', 'posts')}
-            href="/posts/[slug]">
-            <div>
-              {page.latest_post.blog_post.content.banner ? (
-                <img src={page.latest_post.blog_post.content.banner} />
-              ) : null}
-              <div>{page.latest_post.blog_post.content.title}</div>
-              <div>
-                {page.latest_post.blog_post.content.summary ? (
-                  <ReactMarkdown>
-                    {page.latest_post.blog_post.content.summary}
-                  </ReactMarkdown>
-                ) : null}
+          <BlogPostWithImage width="960px">
+            <Link
+              as={page.latest_post.blog_post.path.replace('blog', 'posts')}
+              href="/posts/[slug]">
+              <div className="image-text-sum-wrapper">
+                <div className="image-text-wrapper">
+                  {page.latest_post.blog_post.content.banner ? (
+                    <img src={page.latest_post.blog_post.content.banner} />
+                  ) : null}
+                  <div className="text-wrapper">
+                    <h1>{page.latest_post.blog_post.content.title}</h1>
+                  </div>
+                </div>
+                <div>
+                  {page.latest_post.blog_post.content.summary ? (
+                    <ReactMarkdown>
+                      {page.latest_post.blog_post.content.summary}
+                    </ReactMarkdown>
+                  ) : null}
+                </div>
               </div>
-            </div>
-          </Link>
+            </Link>
+          </BlogPostWithImage>
           <div>
             {page.blocks.map((block) => (
               <div key={block.id}>
