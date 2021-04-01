012efc89366d1293cffb7422a2051c9a7cfdb20d
Goker zafer <gokerlek@gmail.com>
2021-04-02 00:21:15 +0300

    readin minust complated

--- components/reading-minutes.js
+++ components/reading-minutes.js
@@ -1,6 +1,6 @@
 import { FaStopwatch } from 'react-icons/fa';
 
-export const ReadingMinutes = ({ text }) => {
+export const ReadingMinutes = ({ text, small = false }) => {
   return (
     <div className="reading-minutes">
       <FaStopwatch />
@@ -8,7 +8,7 @@ export const ReadingMinutes = ({ text }) => {
       <style jsx>{`
         .reading-minutes {
           color: #a5b1c2;
-          margin: 0.5em 0 0.5em 2.5em;
+          margin: ${small ? 0 : '0.5em 0 0.5em 2.5em'};
           font-size: 0.8em;
           display: flex;
           align-items: center;
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -5,18 +5,17 @@ import { Blocks } from '../../components/blocks';
 import { Banner } from '../../components/banner';
 import { PageHeader } from '../../components/page-header';
 import { useReadingMinutesFromBlocks } from '../../components/use-reading-minutes-from-blocks';
+import { ReadingMinutes } from '../../components/reading-minutes';
 
 const BlogPost = ({ page, menu, footer }) => {
-  const readingMinutesText = useReadingMinutesFromBlocks(
-    page && page.blocks ? page.blocks : [],
-  );
+  const readingMinutesText = useReadingMinutesFromBlocks(page.blocks);
   return (
     <Layout menu={menu.menuItems} footer={footer}>
       <Banner banner={page.banner} />
       <Container>
         <div key={page.key}>
           <PageHeader>{page.title}</PageHeader>
-          <ReadingMinutes text={readingMinutesText} />
+          <ReadingMinutes small text={readingMinutesText} />
           <div>{page.publish_date}</div>
           <Blocks blocks={page.blocks} />
         </div>
