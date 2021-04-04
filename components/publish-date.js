69ca14596034c430e7cf880633eb8c3ca7332120
Goker zafer <gokerlek@gmail.com>
2021-04-04 19:24:57 +0300

    publşsh date with hook

--- components/publish-date.js
+++ components/publish-date.js
@@ -3,15 +3,13 @@ import { FaStopwatch } from 'react-icons/fa';
 export const PublishDate = ({ time }) => {
   return (
     <div className="publish-date">
-      <p>{time}</p>
+      {time}
       <style jsx>{`
         .publish-date {
           color: #a5b1c2;
           font-size: 0.8em;
           display: flex;
           align-items: center;
-        }
-        .publish-date :global(p) {
           margin: 0.1em 0 0.5em;
         }
       `}</style>
--- components/usePublishDate.js
+++ components/usePublishDate.js
@@ -1,9 +1,8 @@
 import { DateTime } from 'luxon';
 
 export const usePublishDate = (page) => {
-  const day = DateTime.fromISO(page.publish_date).setLocale('en-US').day;
-  const month = DateTime.fromISO(page.publish_date).setLocale('en-US').monthShort;
-  const year = DateTime.fromISO(page.publish_date).setLocale('en-US').year;
-  const publishDate = `Published on ${day}th of ${month}, ${year}`;
+  const publishDate = DateTime.fromISO(page.publish_date).toFormat(
+    "'Published on 'dd'th of' LLL,yyyy",
+  );
   return publishDate;
 };
